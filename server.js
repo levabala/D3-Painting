01276f0c510343105d21ce0edbe727f39d779f0f
LevaBala <levabala@ya.ru>
2015-11-25 17:49:20 +0300

    Revert "Next"
    This reverts commit 0653e598ce0df9de10c412cc15b66d410ae40b85.
    
    # Conflicts:
    #	sinus.html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-    sendFile("sinus.html", res);
+    sendFile("index.html", res);
 });
 
 server.listen(1337, '127.0.0.1');
