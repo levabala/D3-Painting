1c14f86f30871ef6e5fbb42a9da82c0d0c50a9ea
LevaBala <levabala@ya.ru>
2015-11-25 16:25:17 +0300

    Sinus rendering
    Perfomance test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-    sendFile("index.html", res);
+    sendFile("sinus.html", res);
 });
 
 server.listen(1337, '127.0.0.1');
